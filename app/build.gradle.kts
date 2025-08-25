78f1ac07cfe184330e1c4aa4b1161ff773c9b2f2
Mehmet Özbek <mehmet.oezbek@freenet.de>
2025-08-25 10:04:24 +0200

    added logo in drawable resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // navigation
+    // navigation
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    // room
+    alias(libs.plugins.kotlin.ksp)
 }
 
 android {
     namespace = "com.example.medreminder"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.medreminder"
@@ -14,6 +19,8 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -49,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +64,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
 }
---
+++ app/src/main/java/com/example/medreminder/ui/AppStart.kt
@@ -0,0 +1,18 @@
+package com.example.medreminder.ui
+ 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable fun AppStart(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppStartPreview() {
+    // Use Theme here
+    AppStart()
+}
